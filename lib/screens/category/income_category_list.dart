58a3aac0b464795e5da8e2e295e2290327c36840
Nidhinlal <lalnidhinp02@gmail.com>
2023-01-24 20:46:23 +0530

    ui

--- lib/screens/category/income_category_list.dart
+++ lib/screens/category/income_category_list.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:simplemoneymanager/colors/colors.dart';
@@ -81,8 +82,9 @@ class IncomeCategoryList extends StatelessWidget {
                             ],
                           ),
                           Center(
-                            child: Text(
+                            child: AutoSizeText(
                               category.name,
+                              maxLines: 4,
                               style: const TextStyle(
                                 fontSize: 24.0,
                               ),

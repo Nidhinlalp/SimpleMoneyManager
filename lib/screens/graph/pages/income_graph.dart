735876e686edae8f4999c5f5756336a2467e788b
Nidhinlal <lalnidhinp02@gmail.com>
2023-01-26 04:02:15 +0530

    ui

--- lib/screens/graph/pages/income_graph.dart
+++ lib/screens/graph/pages/income_graph.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:simplemoneymanager/colors/colors.dart';
-import 'package:simplemoneymanager/db_functions/transaction/transaction_db.dart';
 import 'package:simplemoneymanager/models/cetegory/cetegory_models.dart';
 import 'package:simplemoneymanager/models/transaction/transaction_model.dart';
 import 'package:simplemoneymanager/screens/graph/pages/overview_graph.dart';
